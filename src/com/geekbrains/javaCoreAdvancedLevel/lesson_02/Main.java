d0981cec543e78258f17f6f6df9194d61313e996
Sergey <starovoytov1725@gmail.com>
2020-08-24 13:14:34 +0300

    created MyArraySizeException

--- src/com/geekbrains/javaCoreAdvancedLevel/lesson_02/Main.java
+++ src/com/geekbrains/javaCoreAdvancedLevel/lesson_02/Main.java
@@ -1,4 +1,29 @@
 package com.geekbrains.javaCoreAdvancedLevel.lesson_02;
 
+import java.lang.reflect.Array;
+
 public class Main {
+    public static void main(String[] args) {
+        int[][] myArray = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16},
+                {13, 14, 15, 16}
+        };
+
+        try {
+            methodForCheckingArray(myArray);
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void methodForCheckingArray(int[][] myArray) throws MyArraySizeException {
+        if (myArray.length != 4) {
+            throw new MyArraySizeException();
+        }
+    }
+
+
 }
