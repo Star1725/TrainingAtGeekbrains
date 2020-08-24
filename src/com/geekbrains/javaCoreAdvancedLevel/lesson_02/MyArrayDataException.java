a9f66fbc338b79dbca3144f066f65c450415b850
Sergey <starovoytov1725@gmail.com>
2020-08-24 14:29:05 +0300

    created MyArrayDataException

---
+++ src/com/geekbrains/javaCoreAdvancedLevel/lesson_02/MyArrayDataException.java
@@ -0,0 +1,18 @@
+package com.geekbrains.javaCoreAdvancedLevel.lesson_02;
+
+public class MyArrayDataException extends NumberFormatException{
+
+    private int numStroke;
+    private int numColumn;
+
+    public MyArrayDataException(int numStroke, int numColumn){
+        this.numStroke = numStroke;
+        this.numColumn = numColumn;
+    }
+
+    public MyArrayDataException(String message, int numStroke, int numColumn) {
+        super(message);
+        this.numStroke = numStroke;
+        this.numColumn = numColumn;
+    }
+}
--- src/com/geekbrains/javaCoreAdvancedLevel/lesson_02/MyArraySizeException.java
+++ src/com/geekbrains/javaCoreAdvancedLevel/lesson_02/MyArraySizeException.java
@@ -1,4 +1,7 @@
 package com.geekbrains.javaCoreAdvancedLevel.lesson_02;
 
 public class MyArraySizeException extends Exception{
+    public MyArraySizeException(String message) {
+        super(message);
+    }
 }
